c4d47ef846db7064de88468a42362b78f9375919
Eqmiv <boybingmontemayoriv@gmail.com>
2025-10-08 15:55:15 +0000

    +

--- Soup.java
+++ Soup.java
@@ -1,3 +1,7 @@
+// Eliazar Montemayor IV - This project produce soup that will only contain letters that spell out specific words
+
+
+
 public class Soup {
     //these are instance variables 
     private String letters;
@@ -27,12 +31,15 @@ public class Soup {
 
 //below are the functions you'll be writing.
 
+// PostCondition - Creates a pool of letters called letters
+// PreCondition - It needs the word
     //adds a word to the pool of letters known as "letters"
     public void add(String word){
         letters += word;
     }
 
-
+// PostCondition - Letters has a word in the pool of letters
+// PreCondition - It needs the length of the word
     //Use Math.random() to get a random character from the letters string and return it.
     public String randomLetter(){
     int x = (int)(Math.random()*letters.length());
@@ -40,7 +47,8 @@ public class Soup {
 
     }
 
-
+// PostCondition - Company name exists, puts word in the center
+// PreCondition - It needs the company name and where the center is
     //returns the letters currently stored with the company name placed directly in the center of all
     //the letters
     public String companyCentered(){
@@ -48,18 +56,21 @@ public class Soup {
         return letters.substring(0,letters.length()/2)+company+letters.substring(letters.length()/2);
     }
 
-
+// PostCondition - removes the first vowel that appears in the word
+// PreCondition - Needs a vowel
     //should remove the first available vowel from letters. If there are no vowels this method has no effect.
     public void removeFirstVowel(){
         letters = letters.replaceFirst("[AEIOUaeiou]","");
     }
-
+// PostCondition - removes a random number of letters in a random spot of the word
+// PreCondition - Needs the length of the word, the random number to remove letters, 
     //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
     public void removeSome(int num){
         int x = (int)(Math.random()*(letters.length()-num));
         letters = letters.substring(0,x)+letters.substring(x+num);
     }
-
+// PostCondition - removes the word "word" from the letters, 0therwise it doesn't do anything
+// PreCondition - needs the word "word"
     //should remove the word "word" from the string letters. If the word is not found in letters then it does nothing.
     public void removeWord(String word){
         letters = letters.replaceFirst("word", "");
